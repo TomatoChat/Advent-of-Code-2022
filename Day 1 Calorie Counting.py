8da4ac8d20855bdd94e9413feec351ac3a868857
Andrea Rocco <99046131+TomatoChat@users.noreply.github.com>
2022-12-03 20:22:58 +0100

    Add files via upload

---
+++ Day 1 Calorie Counting.py
@@ -0,0 +1,43 @@
+with open('Day 1 Calorie Counting DATA.txt') as f:
+    lines = f.readlines()
+
+elfNumber = 0
+elvesCalories = [[]]
+
+for x in lines:
+    if x == "\n":
+        elfNumber += 1
+        elvesCalories += [[]]
+    elif x != "\n":
+        elvesCalories[elfNumber] += [int(x)]
+
+totElvesCalories = [sum(x) for x in elvesCalories]
+
+# FIRST ANSWER
+mostCalories = 0
+
+for x in totElvesCalories:
+    if x > mostCalories:
+        mostCalories = x
+
+print(mostCalories)
+
+# SECOND ANSWER
+firstCalories = 0
+secondCalories = 0
+thirdCalories = 0
+
+for x in totElvesCalories:
+    if x >= firstCalories:
+        thirdCalories = secondCalories
+        secondCalories = firstCalories
+        firstCalories = x
+    elif x >= secondCalories:
+        thirdCalories = secondCalories
+        secondCalories = x
+    elif x >= thirdCalories:
+        thirdCalories = x
+
+totalCaloriesTop3 = sum([firstCalories, secondCalories, thirdCalories])
+
+print(totalCaloriesTop3) # 2nd answer
